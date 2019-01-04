f5fbf965a7b0d9a81dce2a76895c5814e29c87d0
oreldm <orel@mip.me>
2019-01-04 13:27:26 +0200

    UploadFile Controller #1

--- gs-rest-service-master/complete/src/main/java/controllers/ArtistAuthController.java
+++ gs-rest-service-master/complete/src/main/java/controllers/ArtistAuthController.java
@@ -11,37 +11,33 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ArtistAuthController {
-	//register
+
 	@RequestMapping("/Artist/Register")
 	public boolean artistRegister(@RequestParam String id, @RequestParam String password) {
-		id=addBrackets(id);
-		password=addBrackets(password);
-		String command="INSERT INTO ARTIST (ARTISTID,PASS) VALUES ("+id+","+password+")";
+		id = addBrackets(id);
+		password = addBrackets(password);
+		String command = "INSERT INTO ARTIST (ARTISTID,PASS) VALUES (" + id + "," + password + ")";
 		System.out.println(command);
-		boolean response=DALService.INSTANCE.sendCommandDoneRecieve(command);
+		boolean response = DALService.INSTANCE.sendCommandDoneRecieve(command);
 		return response;
 	}
-	
-	//boolean verify password
+
 	@RequestMapping("/Artist/VerifyPassword")
-	public boolean verifyPassword( @RequestParam String user,@RequestParam String password) {
-	//	TODO: make it like that select * from artist where ARTISTID='' AND PASS=''
-		user=addBrackets(user);
-		
-		String command="SELECT * FROM ARTIST WHERE user="+user;
+	public boolean verifyPassword(@RequestParam String user, @RequestParam String password) {
+		// TODO: make it like that select * from artist where ARTISTID='' AND PASS=''
+		user = addBrackets(user);
+
+		String command = "SELECT * FROM ARTIST WHERE user=" + user;
 		System.out.println(command);
-		boolean resultBool=DALService.INSTANCE.sendCommand(command);
-		System.out.println("Here is last result : "+DALService.INSTANCE.LAST_RESULT);
-		System.out.println("Here is password : "+password);
-		if(DALService.INSTANCE.LAST_RESULT.equals(password)) {
+		DALService.sendCommand(command);
+		if (DALService.LAST_RESULT.equals(password)) {
 			return true;
 		}
 		return false;
-		
+
 	}
-	
-	
+
 	private static String addBrackets(String str) {
-		return ("'"+str+"'");
+		return ("'" + str + "'");
 	}
 }
--- gs-rest-service-master/complete/src/main/java/controllers/ArtistController.java
+++ gs-rest-service-master/complete/src/main/java/controllers/ArtistController.java
@@ -3,6 +3,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import dal_layer.DALService;
+
 @RestController
 public class ArtistController {
 	
@@ -12,5 +14,25 @@ public class ArtistController {
 	//upload art
 	//delete art
 	//getListOfTags
+	
+	
+	
+	@RequestMapping("/Artist/uploadArt")
+	public boolean artistRegister(@RequestParam String path, @RequestParam String password) {
+		
+		
+		
+		
+		
+//		id=addBrackets(id);
+//		password=addBrackets(password);
+//		String command="INSERT INTO ARTIST (ARTISTID,PASS) VALUES ("+id+","+password+")";
+//		System.out.println(command);
+//		boolean response=DALService.INSTANCE.sendCommandDoneRecieve(command);
+//		return response;
+		
+		return false;
+	}
+	
 
 }
