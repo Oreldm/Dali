7524f48b21f4f8cf78caf2e1d30f6ee529e4d3b1
oreldm <orel@mip.me>
2019-01-04 13:06:58 +0200

    Adding functionality

--- gs-rest-service-master/complete/src/main/java/controllers/ArtistAuthController.java
+++ gs-rest-service-master/complete/src/main/java/controllers/ArtistAuthController.java
@@ -1,6 +1,10 @@
 package controllers;
 
 import dal_layer.DALService;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,16 +12,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ArtistAuthController {
 	//register
-	@RequestMapping("/ArtistRegister")
+	@RequestMapping("/Artist/Register")
 	public boolean artistRegister(@RequestParam String id, @RequestParam String password) {
-		id="'"+id+"'";
-		password="'"+password+"'";
+		id=addBrackets(id);
+		password=addBrackets(password);
 		String command="INSERT INTO ARTIST (ARTISTID,PASS) VALUES ("+id+","+password+")";
 		System.out.println(command);
-		boolean response=DALService.INSTANCE.sendCommand(command);
+		boolean response=DALService.INSTANCE.sendCommandDoneRecieve(command);
 		return response;
 	}
 	
 	//boolean verify password
-
+	@RequestMapping("/Artist/VerifyPassword")
+	public boolean verifyPassword( @RequestParam String user,@RequestParam String password) {
+	//	TODO: make it like that select * from artist where ARTISTID='' AND PASS=''
+		user=addBrackets(user);
+		
+		String command="SELECT * FROM ARTIST WHERE user="+user;
+		System.out.println(command);
+		boolean resultBool=DALService.INSTANCE.sendCommand(command);
+		System.out.println("Here is last result : "+DALService.INSTANCE.LAST_RESULT);
+		System.out.println("Here is password : "+password);
+		if(DALService.INSTANCE.LAST_RESULT.equals(password)) {
+			return true;
+		}
+		return false;
+		
+	}
+	
+	
+	private static String addBrackets(String str) {
+		return ("'"+str+"'");
+	}
 }
--- gs-rest-service-master/complete/src/main/java/dal_layer/DALService.java
+++ gs-rest-service-master/complete/src/main/java/dal_layer/DALService.java
@@ -3,44 +3,81 @@ package dal_layer;
 import java.sql.*;
 
 public class DALService {
-	
-	public static DALService INSTANCE=new DALService();
-	private static Connection con; 
-	private final String PREFIX="jdbc:oracle:thin:@";
-	private final String URL="dalidb.cuznfsjsfeti.eu-west-1.rds.amazonaws.com";
-	private final int PORT=1521;
-	private final String SID="orcl";
-	private final String USERNAME="dali";
-	private final String PASSWORD="Ad123456";
-	
-	
+
+	public static DALService INSTANCE = new DALService();
+	private static Connection con;
+	private static final String PREFIX = "jdbc:oracle:thin:@";
+	private static final String URL = "dalidb.cuznfsjsfeti.eu-west-1.rds.amazonaws.com";
+	private static final int PORT = 1521;
+	private static final String SID = "orcl";
+	private static final String USERNAME = "dali";
+	private static final String PASSWORD = "Ad123456";
+	public static String LAST_RESULT=null;
+
 	private DALService() {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			this.con=DriverManager.getConnection(PREFIX+URL+":"+PORT+":"+SID, USERNAME, PASSWORD);
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static boolean sendCommand(String command) {
+	public static boolean sendCommandDoneRecieve(String command) {
+		DALService.INSTANCE.LAST_RESULT=null;
 		try {
-		// create the statement object
-		Statement stmt = con.createStatement();
+			openConnection();
+			// create the statement object
+			Statement stmt = con.createStatement();
 
-		// execute query
-		ResultSet rs = stmt.executeQuery(command);
-//HOW TO GET FROM STATEMENT:
-//		while (rs.next())
-//			System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+			// execute query
+			ResultSet rs = stmt.executeQuery(command);
+			closeConnection();
 
-		// close the connection object
-		con.close();
-		}catch(Exception e) {
+			// close the connection object
+		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
 		}
 		return true;
+	}
+
+	public static boolean sendCommand(String command) {
+		// create the statement object
+		try {
+			openConnection();
+			Statement stmt = con.createStatement();
+
+			// execute query
+			ResultSet rs = stmt.executeQuery("select * from artist where ARTISTID='orel'");
+			// HOW TO GET FROM STATEMENT:
+			rs.next();
+			LAST_RESULT=rs.getString(2);
+			
+			closeConnection();
+
+			// close the connection object
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	
+	public static void  closeConnection()  {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void openConnection() {
+		try {
+			con = DriverManager.getConnection(PREFIX + URL + ":" + PORT + ":" + SID, USERNAME, PASSWORD);
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
