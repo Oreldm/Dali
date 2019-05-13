dd93a2230ce19bf9d9166116806baed3206fc498
oreldm1 <orelmima@gmail.com>
2019-05-14 00:27:52 +0300

    added likeArtwork and isLikedArtwork

--- Dali-RestAPI/gs-rest-service-master/complete/src/main/java/Controllers/AndroidController.java
+++ Dali-RestAPI/gs-rest-service-master/complete/src/main/java/Controllers/AndroidController.java
@@ -22,22 +22,63 @@ import dal_layer.DALService;
 
 @RestController
 @RequestMapping("/android/")
-public class AndroidController  implements QueryHelper,TableNames   {
-	
+public class AndroidController implements QueryHelper, TableNames {
+
 	@RequestMapping("/login")
 	public boolean login() {
 		return true;
 	}
 
-	
-	
+	@RequestMapping("/likeArtwork")
+	public boolean likeArtwork(@RequestParam(value = "artworkId") int artworkId,
+			@RequestParam(value = "userId") int userId) {
+		if (isLikedArtwork(artworkId, userId)) {
+			return false;
+		}
+
+		String command = "SELECT id from ViewerLikedArtwork ORDER BY id DESC LIMIT 1";
+		ResultSet rs = DALService.sendCommand(command);
+
+		try {
+			int id=1;
+			if (rs.isBeforeFirst()) {
+				rs.next();
+				id = rs.getInt("id") + 1;
+			}else {
+				System.out.println("No data");
+			}
+			String command2 = "INSERT INTO ViewerLikedArtwork (id,artworkId,viewerId) VALUES (" + id + "," + artworkId
+					+ "," + userId + ")";
+			DALService.sendCommandDataManipulation(command2);
+		} catch (Exception e) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@RequestMapping("/isLikeArtwork")
+	public boolean isLikedArtwork(@RequestParam(value = "artworkId") int artworkId,
+			@RequestParam(value = "userId") int userId) {
+		String command = "SELECT * from ViewerLikedArtwork where artworkId=" + artworkId + " AND viewerId=" + userId;
+		ResultSet st = DALService.sendCommand(command);
+		try {
+			if (st.next()) {
+				return true;
+			}
+		} catch (Exception e) {
+		}
+
+		return false;
+	}
+
 	@RequestMapping("/getProfileById")
 	public Viewer getProfileById(@RequestParam(value = "id") int id) {
 		String command = QueryHelper.selectIdFromTable(VIEWER_TABLE, id);
 		ResultSet rs = DALService.sendCommand(command);
-		
+
 		try {
-			Viewer viewer=new Viewer();
+			Viewer viewer = new Viewer();
 			while (rs.next()) {
 				viewer = requestToUserCasting(rs);
 			}
@@ -47,17 +88,17 @@ public class AndroidController  implements QueryHelper,TableNames   {
 			e.printStackTrace();
 		}
 		DALService.closeConnection();
-		
+
 		return null;
 	}
-	
+
 	@RequestMapping("/getArt")
 	public Artwork getArtById(@RequestParam(value = "id") int id) {
 		String command = QueryHelper.selectIdFromTable(ARTWORK_TABLE, id);
 		ResultSet rs = DALService.sendCommand(command);
-		
+
 		try {
-			Artwork artwork=new Artwork();
+			Artwork artwork = new Artwork();
 			while (rs.next()) {
 				artwork = requestToArtworkCasting(rs);
 			}
@@ -67,7 +108,7 @@ public class AndroidController  implements QueryHelper,TableNames   {
 			e.printStackTrace();
 		}
 		DALService.closeConnection();
-		
+
 		return null;
 	}
 
@@ -77,17 +118,17 @@ public class AndroidController  implements QueryHelper,TableNames   {
 		Double xMaxValue = Double.parseDouble(xPosition) + 0.006;
 		Double yMinValue = Double.parseDouble(yPosition) - 0.006;
 		Double yMaxValue = Double.parseDouble(yPosition) + 0.006;
-		String command = "SELECT * from Artwork WHERE Artwork.id IN "
-				+ "(SELECT artworkId FROM Geolocation WHERE "+xMinValue+"<=xPosition AND xPosition<="+
-				xMaxValue+" AND yPosition<="+yMaxValue+" AND "+yMinValue+"<=yPosition) ";
-		
+		String command = "SELECT * from Artwork WHERE Artwork.id IN " + "(SELECT artworkId FROM Geolocation WHERE "
+				+ xMinValue + "<=xPosition AND xPosition<=" + xMaxValue + " AND yPosition<=" + yMaxValue + " AND "
+				+ yMinValue + "<=yPosition) ";
+
 		ResultSet rs = DALService.sendCommand(command);
 		try {
-			List<Artwork> artworkArrList= new ArrayList<Artwork>();
+			List<Artwork> artworkArrList = new ArrayList<Artwork>();
 			while (rs.next()) {
-					Artwork artwork = requestToArtworkCasting(rs);
-					
-					artworkArrList.add(artwork);
+				Artwork artwork = requestToArtworkCasting(rs);
+
+				artworkArrList.add(artwork);
 			}
 			DALService.closeConnection();
 			return artworkArrList;
@@ -97,9 +138,9 @@ public class AndroidController  implements QueryHelper,TableNames   {
 		DALService.closeConnection();
 		return null;
 	}
-	
+
 	private Viewer requestToUserCasting(ResultSet rs) throws Exception {
-		Viewer viewer= new Viewer();
+		Viewer viewer = new Viewer();
 		int id = rs.getInt("id");
 		String name = rs.getString("name");
 		String pictureUrl = rs.getString("picture");
@@ -113,83 +154,80 @@ public class AndroidController  implements QueryHelper,TableNames   {
 		addGeneresToViewer(viewer);
 		addFollowingToViewer(viewer);
 		addLikedArtworkForViewer(viewer);
-		
-		
+
 		return viewer;
 	}
-	
-	private static Viewer addLikedArtworkForViewer(Viewer viewer) throws Exception{
+
+	private static Viewer addLikedArtworkForViewer(Viewer viewer) throws Exception {
 		viewer.setLikedArtwork(getLikedArtworkForViewer(viewer));
 		return viewer;
 	}
-	
-	private static List<Artwork>getLikedArtworkForViewer(Viewer viewer) throws Exception{
-		String command=QueryHelper.selectAllByIdFromTable("ViewerLikedArtwork", "viewerId", viewer.getId());
-		ResultSet rs=DALService.sendCommand(command);
-		List<Artwork>ret=new ArrayList<Artwork>();
-		while(rs.next()) {
-			String command2=QueryHelper.selectIdFromTable(TableNames.ARTWORK_TABLE, rs.getInt("ArtworkId"));
-			ResultSet rs2=DALService.sendCommand(command2);
-			while(rs2.next()) {
+
+	private static List<Artwork> getLikedArtworkForViewer(Viewer viewer) throws Exception {
+		String command = QueryHelper.selectAllByIdFromTable("ViewerLikedArtwork", "viewerId", viewer.getId());
+		ResultSet rs = DALService.sendCommand(command);
+		List<Artwork> ret = new ArrayList<Artwork>();
+		while (rs.next()) {
+			String command2 = QueryHelper.selectIdFromTable(TableNames.ARTWORK_TABLE, rs.getInt("ArtworkId"));
+			ResultSet rs2 = DALService.sendCommand(command2);
+			while (rs2.next()) {
 				ret.add(ArtworkHelper.requestToArtworkCasting(rs2));
 			}
 		}
 		return ret;
 	}
-	
-	private List<Artist> getFollowingToViewer(Viewer viewer) throws Exception{
-		String command = "SELECT * from Artist where id IN (SELECT artistId FROM Viewer_Artist WHERE viewerId=1)";
-		ResultSet rs=DALService.sendCommand(command);
-		
-		List<Artist>artists=new ArrayList<Artist>();
-		while(rs.next()) {
+
+	private List<Artist> getFollowingToViewer(Viewer viewer) throws Exception {
+		String command = "SELECT * from Artist where id IN (SELECT artistId FROM Viewer_Artist WHERE viewerId="
+				+ viewer.getId() + ")";
+		ResultSet rs = DALService.sendCommand(command);
+
+		List<Artist> artists = new ArrayList<Artist>();
+		while (rs.next()) {
 			artists.add(ArtistHelper.requestToArtistCasting(rs));
 		}
-		
+
 		return artists;
 	}
-	
-	private Viewer addFollowingToViewer(Viewer viewer) throws Exception{
+
+	private Viewer addFollowingToViewer(Viewer viewer) throws Exception {
 		viewer.setFollowing(getFollowingToViewer(viewer));
 		return viewer;
 	}
-	
+
 	private List<String> getGeneresToViewer(Viewer viewer) throws Exception {
-		String command = "SELECT name from Tags where id IN "
-				+ "(Select tagId FROM Artwork_Tag where artworkId IN "
-				+ "(SELECT artworkId FROM ViewerLikedArtwork WHERE viewerId="+viewer.getId()+"))";
-		ResultSet rs= DALService.sendCommand(command);
-		
-		List<String>ret=new ArrayList<String>();
-		
-		while(rs.next()) {
+		String command = "SELECT name from Tags where id IN " + "(Select tagId FROM Artwork_Tag where artworkId IN "
+				+ "(SELECT artworkId FROM ViewerLikedArtwork WHERE viewerId=" + viewer.getId() + "))";
+		ResultSet rs = DALService.sendCommand(command);
+
+		List<String> ret = new ArrayList<String>();
+
+		while (rs.next()) {
 			ret.add(rs.getString("name"));
 		}
-		
+
 		return ret;
 	}
-	
-	private Viewer addGeneresToViewer(Viewer viewer) throws Exception{
+
+	private Viewer addGeneresToViewer(Viewer viewer) throws Exception {
 		viewer.setGeneres(getGeneresToViewer(viewer));
 		return viewer;
 	}
 
-
 	private Artwork requestToArtworkCasting(ResultSet rs) throws SQLException {
-		Artwork artwork= new Artwork();
-		int artId=rs.getInt("id");
-		String path= rs.getString("path");
-		String artName=rs.getString("name");
-		int artistId=rs.getInt("artistId");
+		Artwork artwork = new Artwork();
+		int artId = rs.getInt("id");
+		String path = rs.getString("path");
+		String artName = rs.getString("name");
+		int artistId = rs.getInt("artistId");
 		artwork.setId(artId);
 		artwork.setPath(path);
 		artwork.setName(artName);
 		artwork.setArtistId(artistId);
-		JsonObject location= ArtworkHelper.getLocationForArtwork(artwork);
+		JsonObject location = ArtworkHelper.getLocationForArtwork(artwork);
 		artwork.setPositionX(location.get("x").getAsFloat());
 		artwork.setPositionY(location.get("y").getAsFloat());
 		return artwork;
 	}
-	
-	
+
 }
