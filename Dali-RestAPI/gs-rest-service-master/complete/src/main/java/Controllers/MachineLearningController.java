732f87340a0b71713b675aa5591390605b0e8953
oreldm1 <orelmima@gmail.com>
2019-05-14 15:47:28 +0300

    Machine Learning REST Controller

---
+++ Dali-RestAPI/gs-rest-service-master/complete/src/main/java/Controllers/MachineLearningController.java
@@ -0,0 +1,24 @@
+package Controllers;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import dal_layer.DALService;
+
+@RestController
+@RequestMapping("/ML/")
+public class MachineLearningController {
+	@RequestMapping("/postScore")
+	public boolean login(@RequestParam(value = "viewerId") int viewerId,
+			@RequestParam(value = "genereId") int genereId,
+			@RequestParam(value = "score") int score) throws SQLException {
+		String command = "UPDATE ML_Viewer_Tag_Score SET score = score + "+score+" WHERE tagId = "+genereId+" AND viewerId="+viewerId;
+		DALService.sendCommandDataManipulation(command);
+		
+		return true;
+	}
+}
